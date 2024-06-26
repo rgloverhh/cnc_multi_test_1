d9bf86e05831f358574443643ad995fe718f0148
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-06-26 11:13:15 -0700

    Add files via upload

--- cnc_app_multi.py
+++ cnc_app_multi.py
@@ -12,6 +12,7 @@ def load_model(mdl):
 
 model_pcp = load_model('pcp_xg_model.pkl')
 model_cc = load_model('cc_xg_model.pkl')
+model_hc = load_model('heart_xg_model.pkl')
 
 def predict_pcp(input1, input2, input3, input4):
     sl = model_pcp.predict([[input1, input2, input3, input4]])
@@ -21,6 +22,10 @@ def predict_cc(input1, input2, input3, input4):
     sl = model_cc.predict([[input1, input2, input3, input4]])
     return sl[0]
 
+def predict_hc(input1, input2, input3, input4):
+    sl = model_hc.predict([[input1, input2, input3, input4]])
+    return sl[0]
+
 
 def main():
     st.title("CNC Service Level Predictor")
@@ -28,7 +33,7 @@ def main():
     st.caption("Default values are daily average from May 2024")
     st.sidebar.header("Choose your department")
 
-    if st.sidebar.radio("Select department:", ["Primary Care", "Cancer Care"]) == "Primary Care":
+    if st.sidebar.radio("Select department:", ["Primary Care", "Cancer Care", "Heart Care"]) == "Primary Care":
         calls_offered = st.number_input(label="Enter a call volume between 500 and 3000", min_value=500, max_value=4000, step=10, value=1970)
         aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=4.0, max_value=7.0, step=0.05, value=5.50)
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=22.7)
@@ -44,11 +49,11 @@ def main():
             st.subheader("100%")
         else:
             st.subheader(f"{sl_prediction}%")
-        st.caption("Model: eXtreme Gradient Boosting (XGBoost)")
-        st.caption("Data timeframes: 10/3/2022-6/14/2024")
-        st.caption("Current accuracy: 90%")
+        st.sidebar.caption("Model: eXtreme Gradient Boosting (XGBoost)")
+        st.sidebar.caption("Data timeframes: 10/3/2022-6/14/2024")
+        st.sidebar.caption("Current accuracy: 90%")
 
-    else:
+    elif st.sidebar.radio("Select department:", ["Primary Care", "Cancer Care", "Heart Care"]) == "Cancer Care":
         calls_offered = st.number_input(label="Enter a call volume between 300 and 1400", min_value=300, max_value=1400, step=10, value=890)
         aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=5.0, max_value=8.0, step=0.05, value=6.00)
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=25.0)
@@ -64,9 +69,29 @@ def main():
             st.subheader("100%")
         else:
             st.subheader(f"{sl_prediction}%")
-        st.caption("Model: eXtreme Gradient Boosting (XGBoost)") 
-        st.caption("Data timeframes: 6/3/2022-6/19/2024")
-        st.caption("Current accuracy: 86%")
+        st.sidebar.caption("Model: eXtreme Gradient Boosting (XGBoost)") 
+        st.sidebar.caption("Data timeframes: 6/3/2022-6/19/2024")
+        st.sidebar.caption("Current accuracy: 86%")
+
+    elif st.sidebar.radio("Select department:", ["Primary Care", "Cancer Care", "Heart Care"]) == "Heart Care":
+        calls_offered = st.number_input(label="Enter a call volume between 300 and 1400", min_value=300, max_value=1400, step=10, value=796)
+        aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=4.0, max_value=7.0, step=0.05, value=5.25)
+        not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=21.5)
+        ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=7.0, max_value=18.0, step=0.5, value=13.5)
+        not_ready_con = not_ready/100
+        
+        sl_prediction_temp = predict_cc(calls_offered, aht, not_ready_con, ftes_logged_in)
+        sl_prediction = round((sl_prediction_temp*100),1)
+        st.header("Cancer Care Service Level Prediction")
+        if sl_prediction <= 0:
+            st.subheader("0%")
+        elif sl_prediction >= 100:
+            st.subheader("100%")
+        else:
+            st.subheader(f"{sl_prediction}%")
+        st.sidebar.caption("Model: eXtreme Gradient Boosting (XGBoost)") 
+        st.sidebar.caption("Data timeframes: 1/3/2022-6/20/2024")
+        st.sidebar.caption("Current accuracy: 92%")
 
 if __name__ == "__main__":
     main()
