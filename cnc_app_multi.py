5057ccfd114492b4324c1a0e9e445baa1de6eaa4
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-06-28 14:34:32 -0700

    Add files via upload

--- cnc_app_multi.py
+++ cnc_app_multi.py
@@ -13,6 +13,7 @@ def load_model(mdl):
 model_pcp = load_model('pcp_xg_model.pkl')
 model_cc = load_model('cc_xg_model.pkl')
 model_hc = load_model('heart_xg_model.pkl')
+model_ma = load_model('ma_xg_model.pkl')
 
 def predict_pcp(input1, input2, input3, input4):
     sl = model_pcp.predict([[input1, input2, input3, input4]])
@@ -26,13 +27,17 @@ def predict_hc(input1, input2, input3, input4):
     sl = model_hc.predict([[input1, input2, input3, input4]])
     return sl[0]
 
+def predict_ma(input1, input2, input3, input4):
+    sl = model_ma.predict([[input1, input2, input3, input4]])
+    return sl[0]
+
 
 def main():
     st.title("CNC Service Level Predictor")
     st.text("Please fill in the responses below to predict service level")
     st.caption("Default values are daily average from May 2024")
     st.sidebar.header("Choose your department")
-    selected_model = st.sidebar.radio("Select department:", ["Primary Care", "Cancer Care", "Heart Care"])
+    selected_model = st.sidebar.radio("Select department:", ["Primary Care", "Cancer Care", "Heart Care", "MA CRT Team"])
 
     if selected_model == "Primary Care":
         calls_offered = st.number_input(label="Enter a call volume between 500 and 3000", min_value=500, max_value=4000, step=10, value=1970)
@@ -75,7 +80,7 @@ def main():
         st.sidebar.caption("Current accuracy: 86%")
 
     elif selected_model == 'Heart Care':
-        calls_offered = st.number_input(label="Enter a call volume between 300 and 1400", min_value=300, max_value=1400, step=10, value=796)
+        calls_offered = st.number_input(label="Enter a call volume between 300 and 1400", min_value=300, max_value=1400, step=10, value=800)
         aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=4.0, max_value=7.0, step=0.05, value=5.25)
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=21.5)
         ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=7.0, max_value=18.0, step=0.5, value=13.5)
@@ -93,6 +98,26 @@ def main():
         st.sidebar.caption("Model: eXtreme Gradient Boosting (XGBoost)") 
         st.sidebar.caption("Data timeframes: 1/3/2022-6/20/2024")
         st.sidebar.caption("Current accuracy: 92%")
+
+    elif selected_model == 'MA CRT Team':
+        calls_offered = st.number_input(label="Enter a call volume between 200 and 1100", min_value=200, max_value=1100, step=10, value=510)
+        aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=5.0, max_value=9.0, step=0.05, value=6.45)
+        not_ready = st.number_input(label="Not Ready Rate (%)", min_value=20.0, max_value=45.0, step=0.1, value=34.0)
+        ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=4.0, max_value=15.0, step=0.5, value=7.5)
+        not_ready_con = not_ready/100
+        
+        sl_prediction_temp = predict_ma(calls_offered, aht, not_ready_con, ftes_logged_in)
+        sl_prediction = sl_prediction_temp*100
+        st.header("MA CRT Team Service Level Prediction")
+        if sl_prediction <= 0:
+            st.subheader("0%")
+        elif sl_prediction >= 100:
+            st.subheader("100%")
+        else:
+            st.subheader(f"{sl_prediction}%")
+        st.sidebar.caption("Model: eXtreme Gradient Boosting (XGBoost)") 
+        st.sidebar.caption("Data timeframes: 7/3/2023-6/27/2024")
+        st.sidebar.caption("Current accuracy: 86%")
 
 if __name__ == "__main__":
     main()
