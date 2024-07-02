a2ea8be791d05bb853d3499bcb48231ea3ba008e
rgloverhh <156850648+rgloverhh@users.noreply.github.com>
2024-07-02 09:57:11 -0700

    Add files via upload

--- cnc_app_multi.py
+++ cnc_app_multi.py
@@ -45,9 +45,9 @@ def main():
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=22.7)
         ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=20.0, max_value=40.0, step=0.5, value=25.0)
         not_ready_con = not_ready/100
-        
         sl_prediction_temp = predict_pcp(calls_offered, aht, not_ready_con, ftes_logged_in)
         sl_prediction = sl_prediction_temp*100
+        sl_prediction = round(sl_prediction,1)
         st.header("Primary Care Service Level Prediction")
         if sl_prediction <= 0:
             st.subheader("0%")
@@ -59,15 +59,16 @@ def main():
         st.sidebar.caption("Data timeframes: 10/3/2022-6/14/2024")
         st.sidebar.caption("Current accuracy: 90%")
 
+
     elif selected_model == "Cancer Care":
         calls_offered = st.number_input(label="Enter a call volume between 300 and 1400", min_value=300, max_value=1400, step=10, value=890)
         aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=5.0, max_value=8.0, step=0.05, value=6.00)
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=25.0)
         ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=7.0, max_value=17.0, step=0.5, value=12.0)
         not_ready_con = not_ready/100
-        
         sl_prediction_temp = predict_cc(calls_offered, aht, not_ready_con, ftes_logged_in)
         sl_prediction = sl_prediction_temp*100
+        sl_prediction = round(sl_prediction,1)
         st.header("Cancer Care Service Level Prediction")
         if sl_prediction <= 0:
             st.subheader("0%")
@@ -79,15 +80,16 @@ def main():
         st.sidebar.caption("Data timeframes: 6/3/2022-6/19/2024")
         st.sidebar.caption("Current accuracy: 86%")
 
+
     elif selected_model == 'Heart Care':
         calls_offered = st.number_input(label="Enter a call volume between 300 and 1400", min_value=300, max_value=1400, step=10, value=800)
         aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=4.0, max_value=7.0, step=0.05, value=5.25)
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=15.0, max_value=35.0, step=0.1, value=21.5)
         ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=7.0, max_value=18.0, step=0.5, value=13.5)
         not_ready_con = not_ready/100
-        
         sl_prediction_temp = predict_hc(calls_offered, aht, not_ready_con, ftes_logged_in)
         sl_prediction = sl_prediction_temp*100
+        sl_prediction = round(sl_prediction,1)
         st.header("Heart Care Service Level Prediction")
         if sl_prediction <= 0:
             st.subheader("0%")
@@ -99,15 +101,16 @@ def main():
         st.sidebar.caption("Data timeframes: 1/3/2022-6/20/2024")
         st.sidebar.caption("Current accuracy: 92%")
 
+
     elif selected_model == 'MA CRT Team':
         calls_offered = st.number_input(label="Enter a call volume between 200 and 1100", min_value=200, max_value=1100, step=10, value=510)
         aht = st.number_input(label="Average Handle Time (in decimal format, i.e. 5.5 = 5min 30sec -> 0.05 = 3 sec)", min_value=5.0, max_value=9.0, step=0.05, value=6.45)
         not_ready = st.number_input(label="Not Ready Rate (%)", min_value=20.0, max_value=45.0, step=0.1, value=34.0)
         ftes_logged_in = st.number_input(label="Choose the total number of FTEs logged in for the day (use PowerBI CNC Call Metrics Staffing as a guide)", min_value=4.0, max_value=15.0, step=0.5, value=7.5)
         not_ready_con = not_ready/100
-        
         sl_prediction_temp = predict_ma(calls_offered, aht, not_ready_con, ftes_logged_in)
         sl_prediction = sl_prediction_temp*100
+        sl_prediction = round(sl_prediction,1)
         st.header("MA CRT Team Service Level Prediction")
         if sl_prediction <= 0:
             st.subheader("0%")
